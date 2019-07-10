5bf0f836179f374cbfdcf8c26ffa2c2d52255a4d
Orlando Carnate <orlando.carnate@gmail.com>
2019-07-10 02:37:31 -0500

    JS Subclasses using Extend and Super

---
+++ classes/7_subclasses.js
@@ -0,0 +1,29 @@
+// Classes
+class Book {
+    constructor(title, author, year) {
+        this.title = title;
+        this.author = author;
+        this.year = year;
+    }
+
+    getSummary() {
+        return `${this.title} magazine was written by ${this.author}, published ${this.year}`;
+    }
+
+}
+
+
+// Magazine Subclass
+class Magazine extends Book {
+    constructor(title, author, year, month) {
+        super(title, author, year);
+        this.month = month;
+    }
+}
+
+
+// Instantiate Magazine
+
+const mag1 = new Magazine('Wired', 'Various Authors', '2018', 'Jan');
+
+console.log(mag1.getSummary());
